84baa04659fd100d8bd6aa54a609b5532196c7c8
Stephanie Carsten <rcarsten06@gmail.com>
2020-02-04 18:01:26 -0700

    Sign-up form sending to register api

--- disney-app-front-end/src/components/Signup.js
+++ disney-app-front-end/src/components/Signup.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React,  {useState,useEffect} from "react";
 import {useForm} from "react-hook-form"
 import * as yup from "yup"
+import axios from "axios";
+
 
 
 export default function Signup(){
@@ -20,7 +22,15 @@ export default function Signup(){
         validationSchema: SignUpSchema
     });
     const createUser = event =>{
-        console.log(event)
+
+        axios
+            .post('https://backendci-disneyparents.herokuapp.com/api/users/register',event)
+            .then(response=>{
+                console.log(response)
+            })
+            .catch(error =>{
+                console.log(error)
+            })
     }
 
     return(
@@ -33,6 +43,7 @@ export default function Signup(){
                     name="userName" 
                     ref={register} 
                     placeholder="Family Name"
+                    id="userName"
                 />
                 {errors.userName && <p>{errors.userName.message}</p>}
                 <label htmlFor="email">E-mail</label>
@@ -41,6 +52,7 @@ export default function Signup(){
                     name="email" 
                     ref={register} 
                     placeholder="Enter E-mail"
+                    id="email"
                 />
                 {errors.email && <p>{errors.email.message}</p>}
                 <label htmlFor="password">Password</label>
@@ -49,16 +61,61 @@ export default function Signup(){
                     name="password" 
                     ref={register} 
                     placeholder="password"
+                    id="password"
                 />
                 {errors.password && <p>{errors.password.message}</p>}
-                <label htmlFor="password">Number of Children</label>
+                <label htmlFor="location">Location</label>
                 <input 
                     type="text" 
-                    name="noOfChildren" 
+                    name="location" 
                     ref={register} 
-                    placeholder="noOfChildren"
+                    placeholder="location"
+                    id="location"
                 />
-                {errors.noOfChildren && <p>{errors.noOfChildren.message}</p>}
+                {errors.location && <p>{errors.location.message}</p>}
+                <p>Are you a</p>
+                <label htmlFor="parent">Parent</label>
+                <input 
+                    type="radio" 
+                    name="role" 
+                    ref={register}
+                    id="parent"
+                />
+                <p>Are you a</p>
+                <label htmlFor="name">Name</label>
+                <input 
+                    type="text" 
+                    name="name" 
+                    ref={register}
+                    id="name"
+                />
+                <label htmlFor="caregiver">Caregiver</label>
+                <input 
+                    type="radio" 
+                    name="role" 
+                    ref={register}
+                    id="caregiver"
+                />
+                {errors.role && <p>{errors.role.message}</p>}
+                    {/* {document.getElementById("parent").checked?
+                        <>
+                        <label htmlFor="numberOfChildren">Number of children</label>
+                        <input 
+                            type="text" 
+                            name="numberOfChildren" 
+                            ref={register} 
+                            placeholder="numberOfChildren"/>
+                        </>:null}
+                {errors.numberOfChildren && <p>{errors.numberOfChildren.message}</p>}
+                        <label htmlFor="phone">Phone Number</label> */}
+                        
+                        <input 
+                            type="text" 
+                            name="phone" 
+                            ref={register} 
+                            placeholder="phone"
+                        />
+                {errors.phone && <p>{errors.phone.message}</p>}
                 <button>Create Account</button>
             </form>
         </div>
